eb2e8e2518a6e15f48a04ac0588313d3ec46d611
Mark Ambrocio <16071902+markanator@users.noreply.github.com>
2022-02-04 17:13:38 -0700

    adjust for theme and content

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,12 @@ namespace AmbroBlogProject.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
         {
             return View();
         }
