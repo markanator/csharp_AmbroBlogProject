118d62df01d3931a110a3fa88aceb0598a845e5b
Mark Ambrocio <16071902+markanator@users.noreply.github.com>
2022-02-06 13:21:05 -0700

    post tag validation alerts

--- wwwroot/js/postTags.js
+++ wwwroot/js/postTags.js
@@ -2,8 +2,20 @@
 
 function AddTag(){
     var tagEntry = document.getElementById("TagEntry");
-    let newOption = new Option(tagEntry.value, tagEntry.value);
-    document.getElementById("tagList").options[index++] = newOption;
+
+    let searchResult = searchTags(tagEntry.value);
+    if (searchResult != null) {
+        // trigger sweetAlert
+        Swal.fire({
+            title: 'Error!',
+            text: searchResult,
+            icon: 'error',
+            confirmButtonText: 'Continue'
+        });
+    } else {
+        let newOption = new Option(tagEntry.value, tagEntry.value);
+        document.getElementById("tagList").options[index++] = newOption;
+    }
 
     tagEntry.value = "";
     return true;
@@ -11,8 +23,18 @@ function AddTag(){
 
 function DeleteTag() {
     let tagCount = 1;
+    var tagList = document.getElementById('tagList');
+    if (!tagList) return false;
+    if (tagList.selectedIndex == -1) {
+        Swal.fire({
+            title: 'Error!',
+            text: "Please choose a tag before deleting.",
+            icon: 'error',
+            confirmButtonText: 'Continue'
+        });
+        return true;
+    }
     while (tagCount > 0) {
-        const tagList = document.getElementById("tagList");
         let selectedIndex = tagList.selectedIndex;
         if (selectedIndex >= 0) {
             tagList.options[selectedIndex] = null;
@@ -40,4 +62,22 @@ function replaceTag(tag, idx) {
     let newOption = new Option(tag, tag);
     const tagList = document.getElementById("tagList");
     tagList.options[idx] = newOption;
+}
+
+// search function will detect either an empty or a duplicate tag
+// returns error string if error is detected
+
+function searchTags(str) {
+    if (str == "") {
+        return "Empty tags are not permitted.";
+    }
+    var tagsElement = document.getElementById('tagList');
+    if (tagsElement) {
+        let options = tagsElement.options;
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].value == str) {
+                return `The duplicate tag: #${str} will not be added.`;
+            }
+        }
+    }
 }
