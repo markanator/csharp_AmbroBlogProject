2b4193319e61951092b5c57c3a6f53341a1cd32e
Mark Ambrocio <16071902+markanator@users.noreply.github.com>
2022-02-05 23:46:51 -0700

    update email services

--- Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using AmbroBlogProject.Services;
 
 namespace AmbroBlogProject.Areas.Identity.Pages.Account
 {
@@ -22,14 +23,14 @@ namespace AmbroBlogProject.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<BlogUser> _signInManager;
         private readonly UserManager<BlogUser> _userManager;
-        private readonly IEmailSender _emailSender;
+        private readonly IBlogEmailSender _emailSender;
         private readonly ILogger<ExternalLoginModel> _logger;
 
         public ExternalLoginModel(
             SignInManager<BlogUser> signInManager,
             UserManager<BlogUser> userManager,
             ILogger<ExternalLoginModel> logger,
-            IEmailSender emailSender)
+            IBlogEmailSender emailSender)
         {
             _signInManager = signInManager;
             _userManager = userManager;
--- Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using AmbroBlogProject.Services;
 
 namespace AmbroBlogProject.Areas.Identity.Pages.Account.Manage
 {
@@ -18,12 +19,12 @@ namespace AmbroBlogProject.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<BlogUser> _userManager;
         private readonly SignInManager<BlogUser> _signInManager;
-        private readonly IEmailSender _emailSender;
+        private readonly IBlogEmailSender _emailSender;
 
         public EmailModel(
             UserManager<BlogUser> userManager,
             SignInManager<BlogUser> signInManager,
-            IEmailSender emailSender)
+            IBlogEmailSender emailSender)
         {
             _userManager = userManager;
             _signInManager = signInManager;
