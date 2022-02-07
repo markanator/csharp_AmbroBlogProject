ca611a526b39664e0ba35b518ad42aa6565140b7
Mark Ambrocio <16071902+markanator@users.noreply.github.com>
2022-02-06 19:34:59 -0700

    update config

---
+++ Data/ConnectionService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using Npgsql;
+
+namespace AmbroBlogProject.Data
+{
+    public class ConnectionService
+    {
+        public static string GetConnectionString(IConfiguration config) {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+        }
+
+        private static string BuildConnectionString(string databaseUrl)
+        {
+            var dbUri = new Uri(databaseUrl);
+            var userInfo = dbUri.UserInfo.Split(":");
+            return new NpgsqlConnectionStringBuilder()
+            {
+                Host = dbUri.Host,
+                Port = dbUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = dbUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Prefer,
+                TrustServerCertificate = true
+            }.ToString();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,7 @@ namespace AmbroBlogProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(ConnectionService.GetConnectionString(Configuration)));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
